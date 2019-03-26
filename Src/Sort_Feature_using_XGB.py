441fda09b88efdf8e7bccd556be59e881813fe7e
Greatpan <38004594+Greatpanc@users.noreply.github.com>
2019-03-26 22:29:14 +0800

    Add files via upload

--- Src/Sort_Feature_using_XGB.py
+++ Src/Sort_Feature_using_XGB.py
@@ -1,51 +1,14 @@
-#coding=utf-8
+# -*- coding: utf-8 -*- 
+
+'''
+Created on 2019年3月23日
+
+@author: Greatpan
+'''
 
 import pandas as pd
 import xgboost as xgb
 import os,random,cPickle
-
-
-os.mkdir('featurescore')
-
-
-#类别不平衡  2198个正样本（违约），27802个负样本
-
-train = pd.read_csv('../../data/train/train_x_rank.csv')
-train_target = pd.read_csv('../../data/train/train_master.csv',encoding='gb18030')[['Idx','target']]
-train = pd.merge(train,train_target,on='Idx')
-train_y = train.target
-train_x = train.drop(['Idx','target'],axis=1)
-dtrain = xgb.DMatrix(train_x, label=train_y)
-
-test = pd.read_csv('../../data/test/test_x_rank.csv')
-test_Idx = test.Idx
-test = test.drop('Idx',axis=1)
-dtest = xgb.DMatrix(test)
-
-
-train_test = pd.concat([train,test])
-train_test.to_csv('rank_feature.csv',index=None)
-print train_test.shape
-
-"""
-params={
-    	'booster':'gbtree',
-    	'objective': 'rank:pairwise',
-    	'scale_pos_weight': float(len(train_y)-sum(train_y))/float(sum(train_y)),
-        'eval_metric': 'auc',
-    	'gamma':0.1,
-    	'max_depth':6,
-    	'lambda':500,
-        'subsample':0.6,
-        'colsample_bytree':0.3,
-        'min_child_weight':0.2, 
-        'eta': 0.04,
-    	'seed':1024,
-    	'nthread':8
-        }
-xgb.cv(params,dtrain,num_boost_round=1100,nfold=10,metrics='auc',show_progress=3,seed=1024)#733
-
-"""
 
 def pipeline(iteration,random_seed,gamma,max_depth,lambd,subsample,colsample_bytree,min_child_weight):
     params={
@@ -84,8 +47,46 @@ def pipeline(iteration,random_seed,gamma,max_depth,lambd,subsample,colsample_byt
         f.writelines("feature,score\n")
         f.writelines(fs)
 
-
 if __name__ == "__main__":
+    
+    os.mkdir('featurescore')
+    
+    #类别不平衡  2198个正样本（违约），27802个负样本
+    train = pd.read_csv('../../data/train/train_x_rank.csv')
+    train_target = pd.read_csv('../../data/train/train_master.csv',encoding='gb18030')[['Idx','target']]
+    train = pd.merge(train,train_target,on='Idx')
+    train_y = train.target
+    train_x = train.drop(['Idx','target'],axis=1)
+    dtrain = xgb.DMatrix(train_x, label=train_y)
+    
+    test = pd.read_csv('../../data/test/test_x_rank.csv')
+    test_Idx = test.Idx
+    test = test.drop('Idx',axis=1)
+    dtest = xgb.DMatrix(test)
+    
+    train_test = pd.concat([train,test])
+    train_test.to_csv('rank_feature.csv',index=None)
+    print train_test.shape
+    
+    """
+    params={
+            'booster':'gbtree',
+            'objective': 'rank:pairwise',
+            'scale_pos_weight': float(len(train_y)-sum(train_y))/float(sum(train_y)),
+            'eval_metric': 'auc',
+            'gamma':0.1,
+            'max_depth':6,
+            'lambda':500,
+            'subsample':0.6,
+            'colsample_bytree':0.3,
+            'min_child_weight':0.2, 
+            'eta': 0.04,
+            'seed':1024,
+            'nthread':8
+            }
+    xgb.cv(params,dtrain,num_boost_round=1100,nfold=10,metrics='auc',show_progress=3,seed=1024)#733
+    """
+    
     random_seed = range(10000,20000,100)
     gamma = [i/1000.0 for i in range(0,300,3)]
     max_depth = [5,6,7]
